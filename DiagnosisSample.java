f75a70b8e5d36efd2af59a24f1de9483c7ef4865
LoaiNagib <lna202011@gmail.com>
2021-03-20 00:11:26 +0530

    initial commit

---
+++ DiagnosisSample.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diagnosissample;
+
+/**
+ *
+ * @author SabrinaBhat
+ */
+public class DiagnosisSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        newfrm o = new newfrm();
+        o.setVisible(true);
+        
+    }
+    
+}
